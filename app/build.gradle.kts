2945da7a56b74f7449f4de91a364b251c5c62fad
Johnson-Tang2019 <Johnson-tang2019@outlook.com>
2026-06-22 11:32:47 +0800

    Differentiate disabled switch colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.example.accounts"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.0.28"
+        versionCode = 30
+        versionName = "1.0.29"
     }
 
     compileOptions {
